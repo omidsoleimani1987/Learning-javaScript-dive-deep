57bf9d28165c9bebd5d4ad6311f0fd28fcbd8995
omidsoleimani <o.soleimani.ie@gmail.com>
2020-08-07 12:45:27 +0200

    beginning with more on DOM section

---
+++ 09-OOP/08-prototype.js
@@ -0,0 +1,21 @@
+/*
+
+Section 11: Deep Dive: Constructor Functions & Prototypes 
+
+
+- Constructor Functions vs Classes & Understanding "new" --> episode 266 , 267
+
+
+- Introducing __proto__ and Prototypes --> episode 268 , 269 , 270
+
+
+- the global "Object" --> episode 271 , 274
+
+
+-  Methods in Classes & In Constructors --> episode 272 , 273
+
+
+- Setting & Getting Prototypes --> episode 275
+
+
+*/
