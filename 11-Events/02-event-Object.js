4da3bee4ba0bd28dc2901e80b931aa5e7cdc64e0
omidsoleimani <o.soleimani.ie@gmail.com>
2020-08-10 11:10:30 +0200

    starting with the events

---
+++ 11-Events/02-event-Object.js
@@ -0,0 +1,22 @@
+const ourFunctionHandler = function (event) {
+  console.log(event);
+};
+
+// or
+
+const ourFunctionHandler = function (eve) {
+  console.log(eve);
+};
+
+// or
+
+const ourFunctionHandler = function (e) {
+  console.log(e);
+};
+
+btn.addEventListener('click', (e) => {
+  console.log(e.target);
+});
+
+// more on HTML DOM Event Properties and Methods:
+https://www.w3schools.com/jsref/dom_obj_event.asp
