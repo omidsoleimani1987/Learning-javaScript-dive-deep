626738e2870820fec30b25fa123f2be5ad524245
omidsoleimani <o.soleimani.ie@gmail.com>
2020-08-16 16:19:49 +0200

    finished with advanced functions

---
+++ 04-more-on-functions/13-closure.js
@@ -0,0 +1,5 @@
+// see section 15 - episode 328
+// https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/learn/lecture/16183605#overview
+
+// see section 15 - episode 331
+// https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/learn/lecture/16183621#overview
---
+++ 04-more-on-functions/14-recursion.js
@@ -0,0 +1,5 @@
+//! very important concept:
+
+// see section 15 - episode 332 , 333
+
+// https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/learn/lecture/16183623#overview
