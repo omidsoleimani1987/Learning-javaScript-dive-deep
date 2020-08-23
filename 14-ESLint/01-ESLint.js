d39477d7a933c6d82fadd9fd08afedd6717740a6
omidsoleimani <o.soleimani.ie@gmail.com>
2020-08-23 18:23:50 +0200

    adding eslint

---
+++ 14-ESLint/01-ESLint.js
@@ -0,0 +1,32 @@
+/*
+install the extension first in IDE
+then ctrl + shift + P , then find eslint, then enable ESLint
+
+make the npm package : npm init
+
+install it for our project with npm : npm install --save-dev eslint 
+then
+    again ctrl + shift + P , then choose create eslint configuration
+or in terminal
+    ./node_modules/.bin/eslint --init
+
+And then we can define which rules do we want and which not
+ or find some good rules in internet
+
+* *************************************************************************************************************** *
+
+Configuring ESLint
+ESLint offers a lot of different options so that you can really fine-tune it exactly to your requirements.
+
+You can set up your own rules from the ground up (basically what we started doing in the lectures) but you can also use presets and pre-configured rulesets.
+
+To fully understand all options you can configure in .eslintrc.json, this part of the official docs should be helpful: https://eslint.org/docs/user-guide/configuring
+
+To explore all available rules and what they mean, explore this part of the official docs: https://eslint.org/docs/rules/
+
+Want to use a preset? Here you go: https://www.npmjs.com/search?q=eslint-config (just click on one of the results and follow the instructions provided there)
+
+Also check out the docs in general: https://eslint.org/docs/user-guide/getting-started
+
+*/
+// *) https://www.npmjs.com/package/eslint-config-prettier
